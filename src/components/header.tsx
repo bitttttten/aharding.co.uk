a516a43b3ba38d0c4b482341273f1c14da08514d
bitttttten <bitttttten@googlemail.com>
2025-05-11 16:30:00 +0200

    fix: bg opacity for backdrop effect

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
 	return (
 		<HeaderContextProvider>
 			<header className="h-[var(--header-height)] print:hidden">
-				<nav className="fixed z-10 flex h-[var(--header-height)] w-full items-center space-x-[30px] bg-gray-100 bg-opacity-75 px-[24px] backdrop-blur-sm backdrop-saturate-100 md:h-[76px] md:px-[32px] xl:px-[35px]">
+				<nav className="fixed z-10 flex h-[var(--header-height)] w-full items-center space-x-[30px] bg-gray-100/30 bg-opacity-75 px-[24px] backdrop-blur-sm backdrop-saturate-100 md:h-[76px] md:px-[32px] xl:px-[35px]">
 					<NavItem href="/">photos</NavItem>
 					<NavItem href="/sketchbook">sketchbook</NavItem>
 				</nav>
